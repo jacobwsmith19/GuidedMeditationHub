a22d438e891f30cdb667dd50ba264749eed11207
Jake Smith <jacobwsmith19@gmail.com>
2019-05-01 11:04:17 -0500

    added on click functions to create playlists

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,50 +9,87 @@ $.ajax({
     method: "GET"
   }).then(function(response) {
     console.log(response);
-    sheetsA2 = response.sheets[0].data[0].rowData[1].values[0].effectiveValue.stringValue
-    $("#google-sheets").attr("src", sheetsA2);
 
-    console.log("-------------------------------------------------------");
+    // Short ---------------------------------------------------------------------------------------------------------
+    $("#short-button").on("click", function(){
 
-    console.log("Sam Harris:")
-    // function to log the urls for each file on the "sam-harris" tab in the spreadsheet
-    for (i = 1; i < response.sheets[1].data[0].rowData.length; i++){
-        console.log(response.sheets[1].data[0].rowData[i].values[0].effectiveValue.stringValue);
-    }
-    console.log("-------------------------------------------------------");
+        $("#playlist").html("");
 
-    console.log("Sitting:")
-    // function to log the urls for each file on the "sitting" tab in the spreadsheet
-    for (i = 1; i < response.sheets[2].data[0].rowData.length; i++){
-        console.log(response.sheets[2].data[0].rowData[i].values[0].effectiveValue.stringValue);
-    }
-    console.log("-------------------------------------------------------");
+        for (i = 1; i < response.sheets[4].data[0].rowData.length; i++){
+            $("#playlist").append(`
+                <div class='current-playlist' src='${response.sheets[4].data[0].rowData[i].values[0].effectiveValue.stringValue}'>
+                ${response.sheets[4].data[0].rowData[i].values[6].effectiveValue.stringValue}
+                </div>
+                `);
+        }
 
-    console.log("Eyes open:")
-    // function to log the urls for each file on the "eyes-open" tab in the spreadsheet
-    for (i = 1; i < response.sheets[3].data[0].rowData.length; i++){
-        console.log(response.sheets[3].data[0].rowData[i].values[0].effectiveValue.stringValue);
-    }
-    console.log("-------------------------------------------------------");
+        $(".current-playlist").on("click", function(x) {
+            x.preventDefault();
+            $("#currentlyPlaying").attr("src", $(this).attr("src"));
+            $("#audioPlayer")[0].load();
+        });
+    }); 
 
-    console.log("Shorter than 15 minutes:")
-    // function to log the urls for each file on the "shorter-than-1000" tab in the spreadsheet
-    for (i = 1; i < response.sheets[4].data[0].rowData.length; i++){
-        console.log(response.sheets[4].data[0].rowData[i].values[0].effectiveValue.stringValue);
-        $("#playlist").append(`
-            <div class='current-playlist' src='${response.sheets[4].data[0].rowData[i].values[0].effectiveValue.stringValue}'>
-            ${response.sheets[4].data[0].rowData[i].values[6].effectiveValue.stringValue}
-            </div>
-            `);
-    }
-    console.log("-------------------------------------------------------");
+    // Long ---------------------------------------------------------------------------------------------------------
+    $("#long-button").on("click", function(){
 
-    $(".current-playlist").on("click", function(x) {
-        x.preventDefault();
-        $("#currentlyPlaying").attr("src", $(this).attr("src"));
-        console.log(this);
-        $("#audioPlayer")[0].load();
+        $("#playlist").html("");
+
+        for (i = 1; i < response.sheets[5].data[0].rowData.length; i++){
+            $("#playlist").append(`
+                <div class='current-playlist' src='${response.sheets[5].data[0].rowData[i].values[0].effectiveValue.stringValue}'>
+                ${response.sheets[5].data[0].rowData[i].values[6].effectiveValue.stringValue}
+                </div>
+                `);
+        }
+
+        $(".current-playlist").on("click", function(x) {
+            x.preventDefault();
+            $("#currentlyPlaying").attr("src", $(this).attr("src"));
+            $("#audioPlayer")[0].load();
+        });
     });
+
+    // Walking ---------------------------------------------------------------------------------------------------------
+    $("#walking-button").on("click", function(){
+
+        $("#playlist").html("");
+
+        for (i = 1; i < response.sheets[1].data[0].rowData.length; i++){
+            $("#playlist").append(`
+                <div class='current-playlist' src='${response.sheets[1].data[0].rowData[i].values[0].effectiveValue.stringValue}'>
+                ${response.sheets[1].data[0].rowData[i].values[6].effectiveValue.stringValue}
+                </div>
+                `);
+        }
+
+        $(".current-playlist").on("click", function(x) {
+            x.preventDefault();
+            $("#currentlyPlaying").attr("src", $(this).attr("src"));
+            $("#audioPlayer")[0].load();
+        });
+    });
+
+    // Eyes open ---------------------------------------------------------------------------------------------------------
+    $("#eyes-open-button").on("click", function(){
+
+        $("#playlist").html("");
+
+        for (i = 1; i < response.sheets[3].data[0].rowData.length; i++){
+            $("#playlist").append(`
+                <div class='current-playlist' src='${response.sheets[3].data[0].rowData[i].values[0].effectiveValue.stringValue}'>
+                ${response.sheets[3].data[0].rowData[i].values[6].effectiveValue.stringValue}
+                </div>
+                `);
+        }
+
+        $(".current-playlist").on("click", function(x) {
+            x.preventDefault();
+            $("#currentlyPlaying").attr("src", $(this).attr("src"));
+            $("#audioPlayer")[0].load();
+        });
+    });
+    
 
   }); //close AJAX function
 
