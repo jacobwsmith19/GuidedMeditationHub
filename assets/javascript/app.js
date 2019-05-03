4d80860be2e35d2ef8c31520f73b6dfde2a5d089
Jake Smith <jacobwsmith19@gmail.com>
2019-05-03 14:33:13 -0500

    added 'next' and 'previous' buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,9 @@ var shortSheet = 4;
 var longSheet = 5;
 var walkingSheet = 1;
 var eyesOpenSheet = 3;
-
+var playlist = [];
+var playingIndex = 0;
+var content;
 // Hides description box until button is clicked
 $('#description-div').hide();
 
@@ -22,6 +24,7 @@ function playAudio(){
         $("#current-meditation").html("");
         $("#current-meditation").append($(this).attr("title"));
 
+        playingIndex = $(this).attr('data-index');
     });
 }
 
@@ -32,48 +35,93 @@ $.ajax({
     method: "GET"
   }).then(function(response) {
     console.log(response);
-    
-    // Populates playlist div based on button clicked
-    function createPlaylist(){
-        
-        $('#description-div').show();
-        $("#playlist").html("");
+    content = response.sheets;
+}); //close AJAX function
 
-        for (i = 1; i < response.sheets[sheetNum].data[0].rowData.length; i++){
-            $("#playlist").append(`
-                <div class= 'current-playlist' 
-                src= '${response.sheets[sheetNum].data[0].rowData[i].values[0].effectiveValue.stringValue}' 
-                description= '${response.sheets[sheetNum].data[0].rowData[i].values[7].effectiveValue.stringValue}'
-                title= '${response.sheets[sheetNum].data[0].rowData[i].values[6].effectiveValue.stringValue}'>&nbsp; 
-                ${response.sheets[sheetNum].data[0].rowData[i].values[6].effectiveValue.stringValue}
-                </div>
-            `);
-        }
-        playAudio();
+// Short
+$("#short-button").on("click", function(){
+    sheetNum = shortSheet;
+    createPlaylist(shortSheet);
+});
+
+// Long
+$("#long-button").on("click", function(){
+    sheetNum = longSheet;
+    createPlaylist(longSheet);
+}); 
+
+// Walking
+$("#walking-button").on("click", function(){
+    sheetNum = walkingSheet;
+    createPlaylist(walkingSheet);
+}); 
+
+// Eyes open
+$("#eyes-open-button").on("click", function(){
+    sheetNum = eyesOpenSheet;
+    createPlaylist(eyesOpenSheet);
+}); 
+
+$("#next-button").on("click", function(){
+    playingIndex++;
+    if (playingIndex >= playlist.length){
+        playingIndex = 0;
     }
 
-    // Short
-    $("#short-button").on("click", function(){
-        sheetNum = shortSheet;
-        createPlaylist();
-    });
+    var playNext = playlist[playingIndex];
+    $("#currentlyPlaying").attr("src", playNext.src);
+    $("#audioPlayer")[0].load();
 
-    // Long
-    $("#long-button").on("click", function(){
-        sheetNum = longSheet;
-        createPlaylist();
-    }); 
+    // Clears description box, then populates with data from description column in spread sheet
+    $("#description").html("");
+    $("#description").append(playNext.desc);
 
-    // Walking
-    $("#walking-button").on("click", function(){
-        sheetNum = walkingSheet;
-        createPlaylist();
-    }); 
+    // Clears currently playing box, then populates with data from title column in spread sheet
+    $("#current-meditation").html("");
+    $("#current-meditation").append(playNext.title);
+})
 
-    // Eyes open
-    $("#eyes-open-button").on("click", function(){
-        sheetNum = eyesOpenSheet;
-        createPlaylist();
-    }); 
+$("#previous-button").on("click", function(){
+    playingIndex--;
+    if (playingIndex < 0){
+        playingIndex = playlist.length - 1;
+    }
+
+    var playPrev = playlist[playingIndex];
+    $("#currentlyPlaying").attr("src", playPrev.src);
+    $("#audioPlayer")[0].load();
+
+    // Clears description box, then populates with data from description column in spread sheet
+    $("#description").html("");
+    $("#description").append(playPrev.desc);
+
+    // Clears currently playing box, then populates with data from title column in spread sheet
+    $("#current-meditation").html("");
+    $("#current-meditation").append(playPrev.title);
+})
+
+// Populates playlist div based on button clicked
+function createPlaylist(sheetNum){
+    $('#description-div').show();
+    $("#playlist").html("");
+    playlist = [];
+
+    for (i = 1; i < content[sheetNum].data[0].rowData.length; i++){
+        playlist.push({
+            src: content[sheetNum].data[0].rowData[i].values[0].effectiveValue.stringValue,
+            desc: content[sheetNum].data[0].rowData[i].values[7].effectiveValue.stringValue,
+            title: content[sheetNum].data[0].rowData[i].values[6].effectiveValue.stringValue,
+        })
+        $("#playlist").append(`
+            <div class= 'current-playlist' 
+            src= '${content[sheetNum].data[0].rowData[i].values[0].effectiveValue.stringValue}' 
+            description= '${content[sheetNum].data[0].rowData[i].values[7].effectiveValue.stringValue}'
+            title= '${content[sheetNum].data[0].rowData[i].values[6].effectiveValue.stringValue}'
+            data-index='${i-1}'>&nbsp; 
+            ${content[sheetNum].data[0].rowData[i].values[6].effectiveValue.stringValue}
+            </div>
+        `);
+    }
     
-}); //close AJAX function
+    playAudio();
+}
